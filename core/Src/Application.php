fb97f9c35ab72734cb9c6b6b80c7f218a868248b
LoraStratos <LoraStratosSi@mail.ru>
2024-04-10 18:20:11 +0700

    КОНЕЦ

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -18,7 +18,7 @@ class Application
         //Привязываем класс со всеми настройками приложения
         $this->settings = $settings;
         //Привязываем класс маршрутизации с установкой префикса
-        $this->route = new Route($this->settings->getRootPath());
+        $this->route = Route::single()->setPrefix($this->settings->getRootPath());
         //Создаем класс менеджера для базы данных
         $this->dbManager = new Capsule();
         //Создаем класс для аутентификации на основе настроек приложения
@@ -29,6 +29,7 @@ class Application
         //Инициализация класса пользователя на основе настроек приложения
         $this->auth::init(new $this->settings->app['identity']);
     }
+
 
     public function __get($key)
     {
