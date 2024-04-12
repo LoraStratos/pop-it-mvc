cba8b46db5aa583d86801b077ebeb2725f63ca16
LoraStratos <LoraStratosSi@mail.ru>
2024-04-12 11:22:43 +0700

    Css страницы входа в ИС

--- views/site/login.php
+++ views/site/login.php
@@ -1,13 +1,16 @@
-<h2>Авторизация</h2>
-<h3><?= $message ?? ''; ?></h3>
+<div class="div">
 
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
-    ?>
-    <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
+    <form method="post" class="login_form">
+        <h2>Вход</h2>
+        <input type="text" name="login" placeholder="Логин">
+        <input type="password" name="password" placeholder="Пароль">
         <button>Войти</button>
     </form>
+    <h3><?= $message ?? ''; ?></h3>
+
+    <h3><?= app()->auth->user()->name ?? ''; ?></h3>
+    <?php
+    if (!app()->auth::check()):
+    ?>
+</div>
 <?php endif;
